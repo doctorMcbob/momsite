7e7c6fe7013835c2a3c50226adfd80dc8f64123b
doctorMcbob <wootenwesley@gmail.com>
2020-08-03 00:22:43 -0700

    contact page skeleton

--- momsite/momsite.py
+++ momsite/momsite.py
@@ -5,6 +5,9 @@ import waitress
 from pyramid.config import Configurator
 from pyramid.response import Response
 
+import smtplib
+from email.message import EmailMessage
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 templates = Environment(
@@ -19,12 +22,26 @@ def debug(request):
 def main(request):
     return Response(templates.get_template("index.html").render())
 
+def contact(request):
+    return Response(templates.get_template("contact.html").render())
+
+def email(request):
+    print(dir(request))
+    return Response("200 OK")
+
+
 if __name__ == """__main__""":
     with Configurator() as config:
         config.add_route("main", "/")
         config.add_view(main, route_name="main")
         config.add_route("debug", "/debug")
         config.add_view(debug, route_name="debug")
+        config.add_route("contact", "/contact")
+        config.add_view(contact, route_name="contact")
+        config.add_route("email", "/email")
+        config.add_view(email, route_name="email")
+
+        
 
         config.add_static_view(name="/static", path="momsite:/static/")
         config.add_static_view(name="/static/css", path="momsite:/static/css/")
--- momsite/static/js/script.js
+++ momsite/static/js/script.js
@@ -1,1 +1,13 @@
 //this is a script
+function contact_form_submit() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var msg = document.getElementById("message").value;
+    alert("here")
+    xhttp.open("POST", "/email", true);
+    xhttp.setRequestHeader("name", name);
+    xhttp.setRequestHeader("email", email);
+    xhttp.setRequestHeader("message", msg);
+    xhttp.send();
+    return false;
+}

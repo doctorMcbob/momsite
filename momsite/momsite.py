292906cd3880cab3b4624180a18d894ac131316b
doctorMcbob <wootenwesley@gmail.com>
2020-08-09 12:42:47 -0700

    about page and updated pages

--- momsite/momsite.py
+++ momsite/momsite.py
@@ -13,7 +13,7 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 import os
 
 SENDEMAIL = "wootenwesley@gmail.com"
-LOGINEMAIL = "pythontestingground@gmail.com"
+LOGINEMAIL = "pymessager666@gmail.com"
 PASSWORD = os.environ["EMAILPASS"]
 
 templates = Environment(
@@ -27,6 +27,12 @@ def main(request):
 def contact(request):
     return Response(templates.get_template("contact.html").render())
 
+def about(request):
+    return Response(templates.get_template("about.html").render())
+
+def services(request):
+    return Response(templates.get_template("services.html").render())
+
 def email(request):
     try:
         name = request.POST["name"]
@@ -39,12 +45,13 @@ def email(request):
     mail["To"] = SENDEMAIL
     mail["From"] = email
     mail["Subject"] = "Contact from " + str(name)
+    message = "Respond to: " + str(email) + "\n" + message
     mail.set_content(message)
 
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.ehlo()
     s.starttls()
-    
+
     s.login(LOGINEMAIL, PASSWORD)
     
     s.send_message(mail)
@@ -58,6 +65,10 @@ if __name__ == """__main__""":
         config.add_view(main, route_name="main")
         config.add_route("contact", "/contact")
         config.add_view(contact, route_name="contact")
+        config.add_route("services", "/services")
+        config.add_view(services, route_name="services")
+        config.add_route("about", "/about")
+        config.add_view(about, route_name="about")
         config.add_route("email", "/email")
         config.add_view(email, route_name="email")
 
@@ -66,6 +77,7 @@ if __name__ == """__main__""":
         config.add_static_view(name="/static", path="momsite:/static/")
         config.add_static_view(name="/static/css", path="momsite:/static/css/")
         config.add_static_view(name="static/js", path="momsite:/static/js/")
+        config.add_static_view(name="static/img", path="momsite:/static/img/")
 
         config.scan()
         app = config.make_wsgi_app()
